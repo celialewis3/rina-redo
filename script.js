0500cf5b34da2256e49a681671d1ad8e5b84fdee
Celia Lewis <celialewis@umass.edu>
2020-08-06 00:45:11 -0400

    added stuff

---
+++ script.js
@@ -0,0 +1,8 @@
+function changeColor(obj, color) {
+    obj.style.color = color;
+}
+
+function changeBGColor(obj, color, text) {
+    obj.style.backgroundColor = color;
+    obj.style.color = text;
+}
